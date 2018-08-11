35a6672cf3bfe42bcc9f535fe26ab7995e452396
cesar mejia <cesarcalderon909@gmail.com>
2018-08-11 10:49:51 -0600

    database

--- deshuese.sql
+++ deshuese.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 100131
 File Encoding         : 65001
 
-Date: 2018-08-09 13:47:58
+Date: 2018-08-11 10:49:36
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -34,7 +34,7 @@ CREATE TABLE `deshuese` (
   `Costo_Unitario` double DEFAULT NULL,
   `Total_Actual` double DEFAULT NULL,
   PRIMARY KEY (`IdDeshuese`)
-) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of deshuese
@@ -70,6 +70,20 @@ INSERT INTO `deshuese` VALUES ('40', '8785', '86004', 'Posta p/bacon', null, '85
 INSERT INTO `deshuese` VALUES ('41', '8785', '86011', 'Recorte', '63.49', '61.46', '55.5', '0.51', '0.5', '16.344', '907.092', '61.464', '1004.5649999999999');
 INSERT INTO `deshuese` VALUES ('42', '8785', '86013', 'Posta especial', '91.06', '89.32', '80.591', '8.48', '12.11', '272.4', '21952.988', '89.319', '24330.5643');
 INSERT INTO `deshuese` VALUES ('43', '8785', '86108', 'Posta de paleta y pierna cerdo nacional/expo.', '79.63', '78.09', '70.486', '24.71', '30.85', '793.678', '55943.188', '78.094', '61981.660500000005');
+INSERT INTO `deshuese` VALUES ('51', '8772', '86001', 'Grasa', '37.7', '35.42', '34', '9.22', '5.19', '270.13', '9184.42', '35.415', '9566.679252000002');
+INSERT INTO `deshuese` VALUES ('52', '8772', '86002', 'Cuero', '37.66', '35.38', '34', '8.32', '4.68', '243.798', '8289.132', '35.384', '8626.600944');
+INSERT INTO `deshuese` VALUES ('53', '8772', '86003', 'Posta de cerdo', '85.32', '80.14', '77', '33.21', '42.32', '973.376', '74949.952', '80.142', '78008.066656');
+INSERT INTO `deshuese` VALUES ('54', '8772', '86004', 'Posta p/bacon', '85.32', '80.13', '77', '12.95', '16.5', '379.544', '29224.888', '80.134', '30414.2982');
+INSERT INTO `deshuese` VALUES ('55', '8772', '86011', 'Recorte', '61.46', '58.47', '55.5', '0.39', '0.36', '11.35', '629.925', '58.466', '663.5846879999999');
+INSERT INTO `deshuese` VALUES ('56', '8772', '86013', 'Posta especial', '89.32', '83.87', '80.591', '21.73', '28.98', '636.962', '51333.405', '83.865', '53418.567384');
+INSERT INTO `deshuese` VALUES ('57', '8772', '86108', 'Posta de paleta y pierna cerdo nacional/expo.', '78.09', '79.98', '77', '1.55', '1.97', '45.4', '3495.8', '79.984', '3631.2828759999998');
+INSERT INTO `deshuese` VALUES ('58', '8831', '86001', 'Grasa', '35.42', '37.05', '34', '11.25', '6.49', '350.942', '11932.028', '37.053', '13003.593097');
+INSERT INTO `deshuese` VALUES ('59', '8831', '86002', 'Cuero', '35.38', '37.05', '34', '8.25', '4.76', '257.418', '8752.212', '37.05', '9537.304027999999');
+INSERT INTO `deshuese` VALUES ('60', '8831', '86003', 'Posta de cerdo', '80.14', '83.91', '77', '35.38', '46.24', '1104.128', '85017.856', '83.911', '92648.096272');
+INSERT INTO `deshuese` VALUES ('61', '8831', '86004', 'Posta p/bacon', '80.13', '83.92', '77', '12.7', '16.6', '396.342', '30518.334', '83.918', '33260.345980000006');
+INSERT INTO `deshuese` VALUES ('62', '8831', '86011', 'Recorte', '58.47', '60.61', '55.5', '1.09', '1.03', '34.05', '1889.775', '60.609', '2063.7443590000003');
+INSERT INTO `deshuese` VALUES ('63', '8831', '86013', 'Posta especial', '83.87', '87.81', '80.591', '15.64', '21.39', '488.05', '39332.438', '87.814', '42857.759067000006');
+INSERT INTO `deshuese` VALUES ('64', '8831', '86108', 'Posta de paleta y pierna cerdo nacional/expo.', '79.98', '76.79', '70.486', '2.91', '3.48', '90.8', '6400.129', '76.791', '6972.650844');
 
 -- ----------------------------
 -- Table structure for distribucionrecursos
@@ -112,15 +126,15 @@ CREATE TABLE `distribucion_contable` (
   `Inss` double DEFAULT NULL,
   `Costo_MOD` double DEFAULT NULL,
   `Gasto_Indirecto_Libra` double DEFAULT NULL,
-  `Gasto_Indirecto` double DEFAULT NULL,
   `Fecha` date DEFAULT NULL,
   PRIMARY KEY (`Id_Dis_Rec`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of distribucion_contable
 -- ----------------------------
-INSERT INTO `distribucion_contable` VALUES ('1', '4000', '100', '100', '80', '760', '5040', '0.39', null, '2018-08-09');
+INSERT INTO `distribucion_contable` VALUES ('1', '4000', '100', '100', '80', '760', '5040', '0.39', '2018-08-09');
+INSERT INTO `distribucion_contable` VALUES ('2', '3935', '98.375', '98.375', '78.7', '747.65', '4958.1', '0.39', '2018-08-10');
 
 -- ----------------------------
 -- Table structure for encabezado_deshuese
@@ -135,17 +149,20 @@ CREATE TABLE `encabezado_deshuese` (
   `Costo_Total` double DEFAULT NULL,
   `Gasto_MOD` double DEFAULT NULL,
   `GI` double DEFAULT NULL,
+  `Id_Dis_Cont` int(11) DEFAULT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of encabezado_deshuese
 -- ----------------------------
-INSERT INTO `encabezado_deshuese` VALUES ('1', '8775', '2018-07-31', 'descripcion', '2795.75', '175833.82', null, null);
-INSERT INTO `encabezado_deshuese` VALUES ('2', '8798', '2018-07-12', 'Deshuese de cerdo y cortes especiales', '3432.29', '214707.18', null, null);
-INSERT INTO `encabezado_deshuese` VALUES ('3', '8774', '2018-06-25', 'Deshuese de cerdo y cortes especiales', '2849.97', '179243.41', null, null);
-INSERT INTO `encabezado_deshuese` VALUES ('11', '8784', '2018-07-04', 'xvxcvcxvcx', '2634.01', '164711.07', null, null);
-INSERT INTO `encabezado_deshuese` VALUES ('12', '8785', '2018-07-05', 'pierna de cerdo con hueso importada', '3211.77', '200913', '5040', '1063.2788400000002');
+INSERT INTO `encabezado_deshuese` VALUES ('1', '8775', '2018-07-31', 'descripcion', '2795.75', '175833.82', null, null, null);
+INSERT INTO `encabezado_deshuese` VALUES ('2', '8798', '2018-07-12', 'Deshuese de cerdo y cortes especiales', '3432.29', '214707.18', null, null, null);
+INSERT INTO `encabezado_deshuese` VALUES ('3', '8774', '2018-06-25', 'Deshuese de cerdo y cortes especiales', '2849.97', '179243.41', null, null, null);
+INSERT INTO `encabezado_deshuese` VALUES ('11', '8784', '2018-07-04', 'xvxcvcxvcx', '2634.01', '164711.07', null, null, null);
+INSERT INTO `encabezado_deshuese` VALUES ('12', '8785', '2018-07-05', 'pierna de cerdo con hueso importada', '3211.77', '200913', '5040', '1063.2788400000002', '1');
+INSERT INTO `encabezado_deshuese` VALUES ('13', '8772', '2018-06-21', 'Liq pierna de cerdo con hueso importado', '2930.83', '184329.08', '5040', '998.6184000000001', '1');
+INSERT INTO `encabezado_deshuese` VALUES ('14', '8831', '2018-08-07', 'Liq pierna de cerdo con hueso importado', '3120.84', '200363.53', '4958.1', '1061.4747000000002', '2');
 
 -- ----------------------------
 -- Table structure for materiaprima
@@ -306,6 +323,8 @@ ed.Fecha,
 ed.Descripcion_DH,
 ed.Precio_Bruto,
 ed.Costo_Total,
+ed.Gasto_MOD,
+ed.GI,
 d.Materia_Prima,
 d.Descripcion,
 d.Prec_Ant_Kilo,
